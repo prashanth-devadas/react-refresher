51f13a4cb4f7e13de3d4e6e99b6d7fb5bbc7f643
prashanth-devadas <prashanthharidas@yahoo.co.in>
2021-08-06 23:03:23 -0400

    removed some more expense items from the list

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -16,16 +16,6 @@ function Expense(props) {
         amount={props.expense[1].amount}
         date={props.expense[1].date}
       ></ExpenseItem>
-      <ExpenseItem
-        title={props.expense[2].title}
-        amount={props.expense[2].amount}
-        date={props.expense[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expense[3].title}
-        amount={props.expense[3].amount}
-        date={props.expense[3].date}
-      ></ExpenseItem>
     </Card>
   );
 }
